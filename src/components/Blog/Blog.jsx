187d3d70da1335e4d7915df7800bd157ae8e09eb
Your Name <khalidsaifullaha3@gmail.com>
2024-10-21 22:17:55 +0600

    added blogs

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,30 @@
 import PropTypes from 'prop-types';
+import { FaRegBookmark } from 'react-icons/fa';
 
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {cover, title, author_img, author, posted_date, reading_time, hashtags} = blog;
     return (
-        <div>
+        <div className='mb-20'>
+            <img className='w-full rounded-2xl mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div  className='flex mb-8'>
+                   <img className='w-14' src={author_img} alt="" />
+                   <div className='ml-6'>
+                    <h3 className='text-2xl font-semibold'>{author}</h3>
+                    <p>{posted_date}</p>
+                   </div>
+
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button className='ml-2 text-2xl'><FaRegBookmark></FaRegBookmark></button>
+                </div>   
+            </div>
             
+            <h2 className='text-4xl'>{title}</h2>
+            {
+                hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+            }
         </div>
     );
 };
